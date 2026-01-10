3a77f52f25b4110b98e6dd718b2ffb2cfc6f7d18
ManzzyGacor <manzz9293@gmail.com>
2026-01-10 09:33:41 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}", // Scan semua file di dalam folder public
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
